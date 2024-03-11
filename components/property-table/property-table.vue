4f51fabe492faa6487ac4619c55677d345a28b28
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2024-03-11 12:16:07 +0100

    Added overflow table to prevent the `data-table` from overflowing the parent

--- components/property-table/property-table.vue
+++ components/property-table/property-table.vue
@@ -1,44 +1,46 @@
 <template>
-  <vl-data-table mod-zebra>
-    <thead>
-      <tr>
-        <th>Eigenschap</th>
-        <th>Verwacht Type</th>
-        <th>Kardinaliteit</th>
-        <th>Beschrijving</th>
-        <th>Gebruik</th>
-        <th>Codelijst</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="property in properties">
-        <td>
-          <a :href="property?.id" v-vl-tooltip.right="property?.id">{{
-            getLabel(property, 'nl', 'AP')
-          }}</a>
-        </td>
-        <td>
-          <a :href="`#${getLabel(property?.range, 'nl', 'AP')}`">{{
-            getLabel(property?.range, 'nl', 'AP')
-          }}</a>
-        </td>
-        <td>
-          <!-- "CARDINALITY TO ADD" -->
-          <!-- {{ property?.cardinality }} -->
-        </td>
-        <td>
-          {{ getDefinition(property, 'nl', 'AP') }}
-        </td>
-        <td>
-          {{ getUsage(property, 'nl', 'AP') }}
-        </td>
-        <td>
-          <!-- <a :href="property.id">{{
+  <div class="vl-u-table-overflow">
+    <vl-data-table mod-zebra>
+      <thead>
+        <tr>
+          <th>Eigenschap</th>
+          <th>Verwacht Type</th>
+          <th>Kardinaliteit</th>
+          <th>Beschrijving</th>
+          <th>Gebruik</th>
+          <th>Codelijst</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="property in properties">
+          <td>
+            <a :href="property?.id" v-vl-tooltip.right="property?.id">{{
+              getLabel(property, 'nl', 'AP')
+            }}</a>
+          </td>
+          <td>
+            <a :href="`#${getLabel(property?.range, 'nl', 'AP')}`">{{
+              getLabel(property?.range, 'nl', 'AP')
+            }}</a>
+          </td>
+          <td>
+            <!-- "CARDINALITY TO ADD" -->
+            <!-- {{ property?.cardinality }} -->
+          </td>
+          <td>
+            {{ getDefinition(property, 'nl', 'AP') }}
+          </td>
+          <td>
+            {{ getUsage(property, 'nl', 'AP') }}
+          </td>
+          <td>
+            <!-- <a :href="property.id">{{
           }}</a> -->
-        </td>
-      </tr>
-    </tbody>
-  </vl-data-table>
+          </td>
+        </tr>
+      </tbody>
+    </vl-data-table>
+  </div>
 </template>
 
 <script setup lang="ts" name="propertyTable">
